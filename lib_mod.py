cb7a0a41820c265418c69340fe6af3192d699871
blacklight2000 <vphuvan@yahoo.com>
2015-11-14 16:23:19 -0500

    second commit

---
+++ lib_mod.py
@@ -0,0 +1,8 @@
+# module lib_mod
+
+def mult(n1,n2):
+    return n1 * n2
+
+def test_mult_3_4():
+    assert(mult(3,4) == 12)
+
---
+++ main.py
@@ -0,0 +1,7 @@
+##main
+import from lib_mod mult
+
+def main():
+ 
+    mult(5,12)
+
